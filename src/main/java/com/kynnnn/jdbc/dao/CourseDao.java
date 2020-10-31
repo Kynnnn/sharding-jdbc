b6820f00b94891fdabb1d2ff673c4ee05e652027
zhudiwei_13zhfx@126.com <zhudiwei_13zhfx@126.com>
2020-11-01 00:16:12 +0800

    'sharding'

--- src/main/java/com/kynnnn/jdbc/dao/CourseDao.java
+++ src/main/java/com/kynnnn/jdbc/dao/CourseDao.java
@@ -3,10 +3,12 @@ package com.kynnnn.jdbc.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.kynnnn.jdbc.model.Course;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Zhudiwei
  */
 @Mapper
+@Repository
 public interface CourseDao extends BaseMapper<Course> {
 }
--- src/test/java/com/kynnnn/jdbc/ShardingSphereJDBCApplicationTests.java
+++ src/test/java/com/kynnnn/jdbc/ShardingSphereJDBCApplicationTests.java
@@ -1,13 +1,39 @@
 package com.kynnnn.jdbc;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.kynnnn.jdbc.dao.CourseDao;
+import com.kynnnn.jdbc.model.Course;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class ShardingSphereJDBCApplicationTests {
 
+    @Autowired
+    private CourseDao courseDao;
+
     @Test
-    void contextLoads() {
+    void addCourse() {
+        for (int i = 0; i < 10; i++) {
+            Course course = new Course();
+            course.setCname("Java" + i);
+            course.setUserId(100L);
+            course.setCstatus("Normal" + i);
+            courseDao.insert(course);
+        }
+    }
+
+    @Test
+    void findCourse() {
+        QueryWrapper<Course> wrapper = new QueryWrapper<>();
+        wrapper.eq("cid", 529446518545973249L);
+        Course course = courseDao.selectOne(wrapper);
+        System.out.println("course = " + course);
     }
 
 }
